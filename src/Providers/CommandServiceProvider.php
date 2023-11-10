d536f71e0c8fbf22db6bf211436503270ae6b369
Ramin Esmaeilinezhad <rames1363@proton.me>
2023-11-11 01:43:45 +0330

    Initial commit

---
+++ src/Providers/CommandServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Admingate\GitCommitChecker\Providers;
+
+use Admingate\GitCommitChecker\Commands\InstallCommand;
+use Admingate\GitCommitChecker\Commands\PreCommitHookCommand;
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if (! $this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            InstallCommand::class,
+            PreCommitHookCommand::class,
+        ]);
+    }
+}
